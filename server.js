5ea0e422f5a1d09bff6e1382f875faaf575d3ba3
Kotha Santhan Kumar <kothasanthan39@gmail.com>
2025-07-07 19:12:04 +0530

    front-end

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const bookingRoute = require("./routes/bookingRoute");
 // Enable CORS *before* routes
 app.use(cors({
   origin: [
-    'https://hotel-reserve-eta.vercel.app/'
+    'https://hotel-reserve-eta.vercel.app'
   ],
   credentials: true
 }));
