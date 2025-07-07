88697ca821f36753a9eb65f47a7bbbb850a9ef8e
Kotha Santhan Kumar <kothasanthan39@gmail.com>
2025-07-07 18:33:00 +0530

    smtg

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 // Your MongoDB URL
-const mongoURL = process.env.MONGODB_URI;
+const mongoURL = 'mongodb+srv://rishi_db:rishi123@cluster0.cbhlk.mongodb.net/';
 
 mongoose.connect(mongoURL)
   .then(() => console.log('MongoDB connected successfully'))
